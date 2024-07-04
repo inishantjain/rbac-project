e1e67680d0a933755e9fe4311855560c682b403e
inishantjain <nishant.jain12@outlook.com>
2024-07-04 15:21:38 +0530

    edit

--- client/src/pages/AdminPanel/CreateEditUserModal.tsx
+++ client/src/pages/AdminPanel/CreateEditUserModal.tsx
@@ -144,9 +144,9 @@ function CreateEditUserModal({ setModal, getUsers }: UserModalProps) {
             <button
               disabled={loading}
               form="create-user-form"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  shadow-lg shadow-blue-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  shadow-lg shadow-blue-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 capitalize"
             >
-              {loading ? <Spinner /> : "Create User"}
+              {loading ? <Spinner /> : `${modalType} user`}
             </button>
           </div>
         </div>
